5a5aacfd81ff02da0a5ce701fdb7c611b73fb59a
Rishabh Gupta <rishabh.911996@gmail.com>
2023-07-29 13:02:04 -0600

    sum of 2 number

---
+++ javascript/dummyaio/ch3/main.js
@@ -0,0 +1,4 @@
+let firstNumber = prompt("Enter the first number");
+let secondNumber = prompt("Enter the second number");
+let sum = Number(firstNumber) + Number(secondNumber);
+alert("The sum is " + sum);
